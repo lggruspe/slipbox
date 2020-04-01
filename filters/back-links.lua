e3bfdd665bb5b04dfef08be9cc6c5e2e5cceda1c
lggruspe <mail.levig@gmail.com>
2020-04-01 13:33:22 +0800

    Put backlinks in bullet list

--- filters/back-links.lua
+++ filters/back-links.lua
@@ -5,25 +5,23 @@ local metadata = require "zettel.metadata"
 local session = require "zettel.session"
 
 function Pandoc(doc)
-    local para = {}
     local db = sqlite3.open(doc.meta.database)
     local note = session.first_note(db, "title", metadata.get_title(doc.meta)) or {}
     assert(note.filename)
-    if not note.filename then
-        goto cleanup
-    end
+    
+    local blocklists = {}
     for backlink in session.get_backlinks(db, note.filename) do
         local link = backlink.filename
-        local content = (backlink.title or "") .. " (" .. link .. ")"
-        table.insert(para, pandoc.Link(content, link))
-        table.insert(para, pandoc.Str ", ")
+        local content = string.format("%s (%s)", backlink.title or "", link)
+        local inline = pandoc.Link(pandoc.Str(content), link)
+        local block = pandoc.Plain{inline}
+        table.insert(blocklists, {block})
     end
-    if next(para) then
-        table.remove(para)
+
+    if next(blocklists) then
         table.insert(doc.blocks, pandoc.Header(1, pandoc.Str "See also"))
-        table.insert(doc.blocks, pandoc.Para(para))
+        table.insert(doc.blocks, pandoc.BulletList(blocklists))
     end
-::cleanup::
     db:close()
     return doc
 end
