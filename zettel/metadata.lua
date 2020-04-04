bbf560d5c8cd8fde273aaf0deadf595b456dc2b0
Levi Gruspe <mail.levig@gmail.com>
2020-04-04 21:09:34 +0800

    Null check metadata

--- zettel/metadata.lua
+++ zettel/metadata.lua
@@ -7,21 +7,32 @@ function M.get_filename(m)
 end
 
 function M.get_author(m)
+    if not m.author then
+        return nil
+    end
     return pandoc.utils.stringify(m.author)
 end
 
 function M.get_title(m)
+    if not m.title then
+        return nil
+    end
     return pandoc.utils.stringify(m.title)
 end
 
 function M.get_date(m)
+    if not m.date then
+        return nil
+    end
     return pandoc.utils.stringify(m.date)
 end
 
 function M.get_keywords(m)
     local keywords = {}
-    for _, kw in pairs(m.keywords[1].content) do
-        if kw.text then table.insert(keywords, kw.text) end
+    if m.keywords then
+        for _, kw in pairs(m.keywords[1].content) do
+            if kw.text then table.insert(keywords, kw.text) end
+        end
     end
     return keywords
 end
