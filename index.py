108472e3d68bd827edaf549c36032cbfb3a10b5e
Levi Gruspe <mail.levig@gmail.com>
2020-04-07 14:46:15 +0800

    Add basic index generator

--- index.py
+++ index.py
@@ -9,16 +9,28 @@ def get_top_notes():
     conn = sqlite3.connect(Config.database)
     cur = conn.cursor()
     cur.execute("""
-        SELECT filename FROM notes JOIN links ON filename = dest
+        SELECT title, filename, count(*) FROM notes JOIN links ON filename = dest
             GROUP BY filename ORDER BY count(*) DESC
     """)
     rows = cur.fetchall()
     conn.close()
     return rows
 
+def render_index(notes):
+    page = "# Index\n\n"
+    for note in notes:
+        title, relpath, count = note
+        backlink_s = "backlink"
+        if count != 1:
+            backlink_s += "s"
+        page += f"- [{title}]({relpath}) ({count} {backlink_s})\n"
+    return page
+
 def main():
     """Generate index page of posts sorted by number of backlinks."""
     notes = get_top_notes()
+    page = render_index(notes)
+    print(page)
 
 if __name__ == "__main__":
     main()
