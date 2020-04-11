23717a46620fae86805d49a3c14ba590f90a008e
Levi Gruspe <mail.levig@gmail.com>
2020-04-11 23:02:52 +0800

    Pass metadata to Link function by using filter sets

--- filters/html-links.lua
+++ filters/html-links.lua
@@ -1,8 +1,15 @@
 pandoc.utils = require "pandoc.utils"
+local sqlite3 = require "lsqlite3"
 
+local relpath
 local warnings = {}
 
+local function get_relpath(m)
+    relpath = m.relpath
+end
+
 function Link(elem)
+    assert(relpath)
     if elem.target == "" then
         warnings["empty link"] = pandoc.utils.stringify(elem.content)
         elem = elem.content
@@ -12,8 +19,13 @@ function Link(elem)
     return elem
 end
 
-function Meta(m)
+local function show_warnings(m)
     for warning, context in pairs(warnings) do
         io.stderr:write(string.format("Warning: %s in %s (%s)\n", warning, m.relpath, context))
     end
 end
+
+return {
+    { Meta = get_relpath },
+    { Link = Link, Meta = show_warnings }
+}
