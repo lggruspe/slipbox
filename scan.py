5a141ea92113cf8611ae0105ad399cdbc7e79119
Levi Gruspe <mail.levig@gmail.com>
2020-04-12 22:56:06 +0800

    Get notes from command-line args instead of glob

--- scan.py
+++ scan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import glob
+import argparse
 import os
 import sqlite3
 import subprocess as sp
@@ -51,19 +51,24 @@ def init():
     conn.commit()
     conn.close()
 
+def get_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=5000, help="port number")
+    parser.add_argument("note", nargs="*", help="path to note relative to current directory")
+    args = parser.parse_args()
+    return args
+
 def main():
+    args = get_options()
     host = "localhost"
-    port = 5000
-    try:
-        port = int(sys.argv[1])
-    except:
-        pass
+    port = args.port
+    notes = args.note
 
     threading.Thread(target=server.main, args=(host, port)).start()
     init()
 
     tasks = []
-    for note in glob.iglob("**/*.md", recursive=True):
+    for note in notes:
         cmd = scan_metadata(note, port)
         task = sp.Popen(cmd, stdout=sp.DEVNULL, cwd=os.path.dirname(__file__))
         tasks.append(task)
