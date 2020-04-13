f517d6229fac8d05e4490cb672bbaccd2aee1c3d
Levi Gruspe <mail.levig@gmail.com>
2020-04-14 00:32:11 +0800

    Only compile outdated notes

--- compile.py
+++ compile.py
@@ -3,20 +3,37 @@
 import glob
 from os.path import abspath, dirname
 import re
+import sqlite3
 import subprocess as sp
 
 from zettel.config import Config
 from zettel.pandoc.commands import generate_html
 
+def outdated_notes():
+    with sqlite3.connect(Config.database) as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT filename FROM notes WHERE outdated = 1")
+        rows = cur.fetchall()
+    return (row[0] for row in rows)
+
 def main():
-    tasks = []
+    tasks = {}
     database = abspath(Config.database)
-    for note in glob.iglob("**/*.md", recursive=True):
+    for note in outdated_notes():
         cmd = generate_html(note, database)
         task = sp.Popen(cmd, stdout=sp.DEVNULL, cwd=dirname(__file__))
-        tasks.append(task)
-    for task in tasks:
+        tasks[note] = task
+    successes = []
+    for note, task in tasks.items():
         status = task.wait()
+        if status == 0:
+            successes.append(note)
+
+    with sqlite3.connect(Config.database) as conn:
+        cur = conn.cursor()
+        sql = "UPDATE notes SET outdated = 0 WHERE filename = :note"
+        for note in successes:
+            cur.execute(sql, {"note": note})
 
     for filename in glob.iglob("**/*.html", recursive=True):
         pardir = re.sub("(.*)/[^/]*.html", r"\1", abspath(filename))
