3d11af6537be21c4d5fa23ff8ebedb27b23fe452
Levi Gruspe <mail.levig@gmail.com>
2020-04-13 20:35:37 +0800

    Combine find and scan

--- scan.py
+++ scan.py
@@ -1,26 +1,55 @@
 #!/usr/bin/env python3
 
 import argparse
+import glob
 import os.path
+import sqlite3
 import subprocess as sp
 import sys
 import threading
+import time
 
 from zettel import client, server
+from zettel.config import Config
 from zettel.pandoc.commands import scan_metadata
 
 def get_options():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, default=5000, help="port number")
-    parser.add_argument("note", nargs="*", help="path to note relative to current directory")
     args = parser.parse_args()
     return args
+
+def get_last_scan():
+    conn = sqlite3.connect(Config.database)
+    cur = conn.cursor()
+    cur.execute("SELECT value FROM meta WHERE key = 'last_scan'")
+    last_scan = int(cur.fetchone()[0])
+    conn.close()
+    return last_scan
+
+def update_last_scan():
+    conn = sqlite3.connect(Config.database)
+    cur = conn.cursor()
+    cur.execute("""
+        INSERT INTO meta (key, value) VALUES ('last_scan', :last_scan)
+            ON CONFLICT (key) DO UPDATE SET value = :last_scan
+    """, {"last_scan": time.time()})
+    conn.commit()
+    conn.close()
+
+def notes_modified_recently(last_scan=None):
+    if last_scan is None:
+        last_scan = get_last_scan()
+    for note in glob.iglob("**/*.md", recursive=True):
+        mtime = os.path.getmtime(note)
+        if mtime >= last_scan:
+            yield note
 
 def main():
     args = get_options()
     host = "localhost"
     port = args.port
-    notes = args.note
+    notes = list(notes_modified_recently())
     if not notes:
         return
 
@@ -34,6 +63,7 @@ def main():
     for task in tasks:
         task.wait()
     client.shutdown(host, port)
+    update_last_scan()
 
 if __name__ == "__main__":
     main()
