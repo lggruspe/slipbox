473b84b3a63dd3a13092f10f331788da526125c5
Levi Gruspe <mail.levig@gmail.com>
2020-04-20 19:50:34 +0800

    Use Makefile instead of compile.py to generate html

--- zettel/pandoc/commands.py
+++ zettel/pandoc/commands.py
@@ -10,13 +10,3 @@ def scan_metadata(note, port):
     filters = lua_filter("zettel-scan.lua")
     filename = os.path.join(BASEDIR, note)
     return pandoc(meta, filters, filename)
-
-def generate_html(note, database):
-    meta = metadata(basedir=BASEDIR, relpath=note, database=database)
-    filename = os.path.join(BASEDIR, note)
-    html = re.sub("(.*).md", r"\1.html", filename)
-    lua_filters = lua_filter("zettel-compile.lua")
-    filters = pandoc_filter("pandoc-citeproc")
-    bib = bibliography(os.path.abspath("zettel.bib"))
-    return pandoc("-s -c zettel.css", meta, lua_filters, bib, filters, filename,
-        "-o", html)
