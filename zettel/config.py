04a48a9a24b9b39fbba564413f47ba3035735733
Levi Gruspe <mail.levig@gmail.com>
2020-05-05 01:13:25 +0800

    Add variables to user config object

--- zettel/config.py
+++ zettel/config.py
@@ -1,8 +1,18 @@
 from dataclasses import dataclass
+from os.path import abspath, curdir, dirname, join, pardir
 
 @dataclass
 class UserConfig:
+    pandoc: str = "pandoc"
+    python: str = "python3"
+    bib: str = "zettel.bib"
+    css: str = "zettel.css"
     database: str = "zettel.db.sqlite3"
+    options: str = "--mathjax --strip-comments"
+    basedir: str = abspath(curdir)  #
+    filters: str = "--lua-filter={} -Pandoc-citeproc".format(
+        abspath(join(dirname(__file__), pardir, "filters", "zettel-compile.lua"))
+    )
 
 @dataclass
 class Config:
