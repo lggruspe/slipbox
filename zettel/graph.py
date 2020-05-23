9858e2f1f2c8ba3be095c386602a37768f6e8d1c
Levi Gruspe <mail.levig@gmail.com>
2020-05-24 00:19:01 +0800

    Add command for visualizing folgezettels

---
+++ zettel/graph.py
@@ -0,0 +1,50 @@
+import re
+import sqlite3
+import sys
+import networkx as nx
+from networkx.drawing.nx_pydot import write_dot
+from .config import Config
+
+class Folgezettel:
+    ntrail = re.compile(r"^(.*?)\d+$")
+    atrail = re.compile(r"^(.*?)[a-zA-Z]+$")
+
+    @staticmethod
+    def parent(note):
+        result = re.sub(Folgezettel.atrail, r"\1", note)
+        if note != result:
+            return result
+        result = re.sub(Folgezettel.ntrail, r"\1", note)
+        return result if note != result else None
+
+def graph_links(pattern, output=sys.stdout):
+    """Create dot graph of notes."""
+    config = Config()
+    sql = "SELECT src, dest FROM links WHERE src LIKE :pattern"
+    G = nx.DiGraph()
+    with sqlite3.connect(config.user.database) as conn:
+        cur = conn.cursor()
+        for src, dest in cur.execute(sql, {"pattern": pattern}):
+            G.add_edge(src, dest)
+    write_dot(G, output)
+
+def graph_folgezettels(pattern, output=sys.stdout):
+    """Create dot graph of sequence notes."""
+    config = Config()
+    sql = """
+        SELECT note, outline, seqnum FROM folgezettels
+            WHERE outline LIKE :pattern
+    """
+    notes = {}
+    with sqlite3.connect(config.user.database) as conn:
+        cur = conn.cursor()
+        for note, outline, seqnum in cur.execute(sql, {"pattern": pattern}):
+            notes[(outline, seqnum)] = note
+
+    G = nx.DiGraph()
+    for (outline, seqnum), note in notes.items():
+        par_id = Folgezettel.parent(seqnum)
+        parent = notes.get((outline, par_id))
+        if parent:
+            G.add_edge(f"{par_id} - {parent}", f"{seqnum} - {note}")
+    write_dot(G, output)
--- zk.py
+++ zk.py
@@ -1,7 +1,9 @@
+import sys
 from zettel.build import build
 from zettel.caps import *
 from zettel.config import Config
 from zettel.genin import generate_ninja
+from zettel.graph import graph_folgezettels, graph_links
 from zettel.scan import scan_zettels, show_missing
 
 def main(config=Config()):
@@ -14,11 +16,17 @@ def main(config=Config()):
                help=f"port number (default={config.port!r})")
     output = arg("-o", "--output", type=str, default="build.ninja",
                  help="output ninja file (default='build.ninja')")
+    pattern = arg("-p", "--pattern", type=str, default='%%',
+                  help="filename SQL pattern (default='%%')")
+    dot = arg("-o", "--output", type=str, dest="output", default=sys.stdout,
+              help="output dot file (default=stdout)")
     cmd = Cmd(desc("zk", "Manage zettelkasten notes."),
               Cmd(scan_zettels, db, host, port, prog="scan"),
               Cmd(generate_ninja, db, output, prog="genin"),
               Cmd(show_missing, db, prog="missing"),
-              Cmd(build, db, host, port, output))
+              Cmd(build, db, host, port, output),
+              Cmd(graph_folgezettels, pattern, dot, prog="gfz"),
+              Cmd(graph_links, pattern, dot, prog="graph"))
     cmd.run()
 
 if __name__ == "__main__":
