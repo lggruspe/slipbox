69c988d4434da239c6bde2f962a3464b8c032d58
Levi Gruspe <mail.levig@gmail.com>
2020-05-30 13:01:26 +0800

    Remove one-second sleep in build

--- zettel/build.py
+++ zettel/build.py
@@ -1,3 +1,4 @@
+import multiprocessing as mp
 import subprocess as sp
 from time import sleep
 from .genin import generate_ninja
@@ -5,7 +6,10 @@ from .scan import scan_zettels
 
 def build(database, host, port, output="build.ninja"):
     """Generate HTML files automatically."""
-    scan_zettels(database, host, port)
-    sleep(1)
+    # for some reason scan_zettels(database, host, port) alone doesn't write to
+    # the database
+    t = mp.Process(target=scan_zettels, args=(database, host, port))
+    t.start()
+    t.join()
     generate_ninja(database, output)
     sp.run(f"ninja -f {output}".split())
