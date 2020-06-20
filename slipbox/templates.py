934511ae7272e9794575980f5050d8ccaba68e6f
Levi Gruspe <mail.levig@gmail.com>
2020-06-20 22:00:45 +0800

    Add module for creating DOM elements in python

---
+++ slipbox/templates.py
@@ -0,0 +1,24 @@
+"""HTML elements."""
+
+from textwrap import indent
+
+class Elem:
+    """Represents an HTML element."""
+    def __init__(self, tag, *children, **attributes):
+        self.tag = tag
+        self.children = children
+        self.attributes = attributes.copy()
+
+def render(elem, prefix=""):
+    """Render HTML from Elem or str."""
+    if isinstance(elem, str):
+        return prefix + elem
+    attrs = ""
+    for key, val in elem.attributes.items():
+        attrs += f" {key}={val!r}"
+    children = indent("\n".join(render(child) for child in elem.children),
+                      prefix + "  ")
+    return "<{tag}{attrs}>\n{children}\n</{tag}>".format(
+        tag=elem.tag,
+        attrs=attrs,
+        children=children)
---
+++ slipbox/test_templates.py
@@ -0,0 +1,31 @@
+"""Test templates.py."""
+
+from .templates import Elem, render
+
+def test_render_text():
+    """String must render as itself."""
+    text = "text"
+    assert render(text) == text
+
+def test_render_element():
+    """Element must render with children and attributes."""
+    elem = Elem("a", "link", href="#")
+    assert render(elem) == """<a href='#'>
+  link
+</a>"""
+
+def test_render_nested():
+    """Nested element must render with proper indentation."""
+    elem = Elem("p", Elem("a", "link", href="#"))
+    assert render(elem) == """<p>
+  <a href='#'>
+    link
+  </a>
+</p>"""
+
+def test_render_no_attributes():
+    """Element with no attributes must have no space after tag."""
+    elem = Elem("p", "text")
+    assert render(elem) == """<p>
+  text
+</p>"""
