f215f6a37dd7c78d58dfbf8c430bf8e0f2fea962
Levi Gruspe <mail.levig@gmail.com>
2020-06-20 23:56:45 +0800

    Cleanup javascript

--- slipbox/data/seealso.js
+++ slipbox/data/seealso.js
@@ -1,5 +1,5 @@
-function A(innerText, href, title) {
-  let a = document.createElement("a")
+function A (innerText, href, title) {
+  const a = document.createElement('a')
   a.innerText = innerText
   a.href = href
   if (title != null) {
@@ -8,141 +8,141 @@ function A(innerText, href, title) {
   return a
 }
 
-function Li(children) {
-  let li = document.createElement("li")
-  for (let child of children) {
+function Li (children) {
+  const li = document.createElement('li')
+  for (const child of children) {
     li.appendChild(child)
   }
   return li
 }
 
-function* generateBacklinkLis(slipbox, id) {
+function * generateBacklinkLis (slipbox, id) {
   console.assert(slipbox)
   console.assert(slipbox.notes)
-  console.assert(typeof(id) === "number")
-  let notes = slipbox.notes
-  let note = notes[id] || {backlinks: []}
-  for (let link of note.backlinks) {
-    console.assert(typeof(link.src) === "number")
+  console.assert(typeof (id) === 'number')
+  const notes = slipbox.notes
+  const note = notes[id] || { backlinks: [] }
+  for (const link of note.backlinks) {
+    console.assert(typeof (link.src) === 'number')
     console.assert(notes[link.src])
-    console.assert(typeof(notes[link.src].title) === "string")
+    console.assert(typeof (notes[link.src].title) === 'string')
     console.assert(notes[link.src].title)
     yield Li([
-      document.createTextNode("[" + link.src + "] "),
-      A(notes[link.src].title, "#" + link.src, link.annotation)
-      ])
+      document.createTextNode('[' + link.src + '] '),
+      A(notes[link.src].title, '#' + link.src, link.annotation)
+    ])
   }
 }
 
-function* generateParentLis(slipbox, id) {
+function * generateParentLis (slipbox, id) {
   console.assert(slipbox)
   console.assert(slipbox.notes)
-  console.assert(typeof(id) === "number")
-  let notes = slipbox.notes
-  let note = notes[id] || {aliases: []}
-  for (let alias of note.aliases) {
+  console.assert(typeof (id) === 'number')
+  const notes = slipbox.notes
+  const note = notes[id] || { aliases: [] }
+  for (const alias of note.aliases) {
     console.assert(slipbox.aliases[alias])
-    let parentAlias = slipbox.aliases[alias].parent
+    const parentAlias = slipbox.aliases[alias].parent
     if (parentAlias) {
-      let parentId = slipbox.aliases[parentAlias].id
-      console.assert(typeof(parentId) === "number")
+      const parentId = slipbox.aliases[parentAlias].id
+      console.assert(typeof (parentId) === 'number')
       yield Li([
-        document.createTextNode("[" + parentAlias + "] "),
-        A(slipbox.notes[parentId].title, "#" + parentId)
-        ])
+        document.createTextNode('[' + parentAlias + '] '),
+        A(slipbox.notes[parentId].title, '#' + parentId)
+      ])
     }
   }
 }
 
-function* generateChildLis(slipbox, id) {
+function * generateChildLis (slipbox, id) {
   console.assert(slipbox)
   console.assert(slipbox.notes)
-  console.assert(typeof(id) === "number")
-  let notes = slipbox.notes
-  let note = notes[id] || {aliases: []}
-  for (let alias of note.aliases) {
+  console.assert(typeof (id) === 'number')
+  const notes = slipbox.notes
+  const note = notes[id] || { aliases: [] }
+  for (const alias of note.aliases) {
     console.assert(slipbox.aliases[alias])
-    for (let child of slipbox.aliases[alias].children) {
-      let childId = slipbox.aliases[child].id
+    for (const child of slipbox.aliases[alias].children) {
+      const childId = slipbox.aliases[child].id
       yield Li([
-        document.createTextNode("[" + child + "] "),
-        A(slipbox.notes[childId].title, "#" + childId)
-        ])
+        document.createTextNode('[' + child + '] '),
+        A(slipbox.notes[childId].title, '#' + childId)
+      ])
     }
   }
 }
 
-function createRelatedUl(slipbox, id) {
-  let ul = document.createElement("ul")
-  for (let li of generateBacklinkLis(slipbox, id)) {
+function createRelatedUl (slipbox, id) {
+  const ul = document.createElement('ul')
+  for (const li of generateBacklinkLis(slipbox, id)) {
     ul.appendChild(li)
   }
-  for (let li of generateParentLis(slipbox, id)) {
+  for (const li of generateParentLis(slipbox, id)) {
     ul.appendChild(li)
   }
-  for (let li of generateChildLis(slipbox, id)) {
+  for (const li of generateChildLis(slipbox, id)) {
     ul.appendChild(li)
   }
   return ul
 }
 
-function createSeeAlsoDiv(slipbox, id) {
-  let div = document.createElement("div")
-  div.id = "slipbox-see-also"
-  let ul = createRelatedUl(slipbox, id)
+function createSeeAlsoDiv (slipbox, id) {
+  const div = document.createElement('div')
+  div.id = 'slipbox-see-also'
+  const ul = createRelatedUl(slipbox, id)
   if (ul.children.length > 0) {
-    let h3 = document.createElement("h3")
-    h3.innerText = "See also"
+    const h3 = document.createElement('h3')
+    h3.innerText = 'See also'
     div.appendChild(h3)
     div.appendChild(ul)
   }
   return div
 }
 
-function createAliasesP(slipbox, id) {
+function createAliasesP (slipbox, id) {
   console.assert(slipbox)
   console.assert(slipbox.notes)
-  console.assert(typeof(id) == "number")
-  let note = slipbox.notes[id] || {aliases: []}
-  let p = document.createElement("p")
-  p.id = "slipbox-aliases"
-  let aliases = note.aliases.map(alias => document.createTextNode(" ["+alias+"]"))
+  console.assert(typeof (id) === 'number')
+  const note = slipbox.notes[id] || { aliases: [] }
+  const p = document.createElement('p')
+  p.id = 'slipbox-aliases'
+  const aliases = note.aliases.map(alias => document.createTextNode(' [' + alias + ']'))
   if (aliases.length > 0) {
-    let text = document.createElement("strong")
-    text.innerText = "Aliases:"
+    const text = document.createElement('strong')
+    text.innerText = 'Aliases:'
     p.appendChild(text)
     aliases.forEach(alias => p.appendChild(alias))
   }
   return p
 }
 
-function setAliasesP(slipbox, id) {
-  let p = document.getElementById("slipbox-aliases")
+function setAliasesP (slipbox, id) {
+  const p = document.getElementById('slipbox-aliases')
   if (p) {
     p.remove()
   }
   document.body.appendChild(createAliasesP(slipbox, id))
 }
 
-function setSeeAlsoDiv(slipbox, id) {
-  let div = document.getElementById("slipbox-see-also")
+function setSeeAlsoDiv (slipbox, id) {
+  const div = document.getElementById('slipbox-see-also')
   if (div) {
     div.remove()
   }
   document.body.appendChild(createSeeAlsoDiv(slipbox, id))
 }
 
-function seeAlso(slipbox, hash) {
-  let section = getSectionFromHash(hash)  // closest level1 ancestor
-  let id = section ? Number(section.id) : 0
+function seeAlso (slipbox, hash) {
+  const section = getSectionFromHash(hash) // closest level1 ancestor
+  const id = section ? Number(section.id) : 0
   setAliasesP(slipbox, id)
   setSeeAlsoDiv(slipbox, id)
 }
 
-window.addEventListener("hashchange", function() {
+window.addEventListener('hashchange', function () {
   seeAlso(slipbox, window.location.hash)
 })
 
-window.addEventListener("load", function() {
+window.addEventListener('load', function () {
   seeAlso(slipbox, window.location.hash)
 })
--- slipbox/data/toggle.js
+++ slipbox/data/toggle.js
@@ -1,59 +1,59 @@
-function hideSections() {
-  let sections = document.getElementsByTagName("section")
+function hideSections () {
+  const sections = document.getElementsByTagName('section')
   for (let i = 0; i < sections.length; i++) {
-    let section = sections[i]
-    if (section.classList.contains("level1")) {
+    const section = sections[i]
+    if (section.classList.contains('level1')) {
       if (Number.isInteger(Number(section.id))) {
-        section.style.display = "none"
-      } else if (section.id.charAt(0) == '#') {
-        section.style.display = "none"
-      } else if (section.id == "references") {
-        section.style.display = "none"
-      } else if (section.id.slice(0, 4) == "ref-") {
-        section.style.display = "none"
-      } else if (section.id == "tags") {
-        section.style.display = "none"
+        section.style.display = 'none'
+      } else if (section.id.charAt(0) === '#') {
+        section.style.display = 'none'
+      } else if (section.id === 'references') {
+        section.style.display = 'none'
+      } else if (section.id.slice(0, 4) === 'ref-') {
+        section.style.display = 'none'
+      } else if (section.id === 'tags') {
+        section.style.display = 'none'
       }
     }
   }
 }
 
-function getSectionFromHash(hash) {
-  let id = hash.substring(1)
+function getSectionFromHash (hash) {
+  const id = hash.substring(1)
   if (!id) { return null }
-  let elem = document.getElementById(id)
+  const elem = document.getElementById(id)
   if (elem) {
-    return elem.closest("section.level1")
+    return elem.closest('section.level1')
   }
 }
 
 let _previousHash = window.location.hash
-function changeSection() {
-  let oldSection = getSectionFromHash(_previousHash)
+function changeSection () {
+  const oldSection = getSectionFromHash(_previousHash)
   if (oldSection) {
-    oldSection.style.display = "none"
+    oldSection.style.display = 'none'
   }
   _previousHash = window.location.hash
-  let newSection = getSectionFromHash(_previousHash)
+  const newSection = getSectionFromHash(_previousHash)
   if (newSection) {
-    newSection.style.display = ""
-    document.title = newSection.title || "Slipbox"
+    newSection.style.display = ''
+    document.title = newSection.title || 'Slipbox'
   } else {
-    window.location.hash = "#0"
+    window.location.hash = '#0'
   }
 }
 
-function addNotFoundSection() {
-  let section = document.createElement("section")
-  section.id = "0"
-  section.classList.add("level1")
-  let h1 = document.createElement("h1")
-  h1.innerText = "Note not found"
+function addNotFoundSection () {
+  const section = document.createElement('section')
+  section.id = '0'
+  section.classList.add('level1')
+  const h1 = document.createElement('h1')
+  h1.innerText = 'Note not found'
   section.appendChild(h1)
   document.body.appendChild(section)
 }
 
-window.addEventListener("load", addNotFoundSection)
-window.addEventListener("load", hideSections)
-window.addEventListener("load", changeSection)
-window.addEventListener("hashchange", changeSection)
+window.addEventListener('load', addNotFoundSection)
+window.addEventListener('load', hideSections)
+window.addEventListener('load', changeSection)
+window.addEventListener('hashchange', changeSection)
