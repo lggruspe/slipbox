4ee84c515f600565a1cb11ac694b1141b971e09f
Levi Gruspe <mail.levig@gmail.com>
2020-07-12 14:22:20 +0800

    Cache base64 encodings

--- slipbox/filters/base64.lua
+++ slipbox/filters/base64.lua
@@ -1,11 +1,18 @@
 local basexx = require "basexx"
 
+local cache = {}
 local function file_to_base64(filename)
+  local base64 = cache[filename]
+  if base64 then return base64 end
+
   local file = io.open(filename)
   if file == nil then return "" end
   local content = file:read"*a"
   file:close()
-  return basexx.to_base64(content)
+
+  base64 = basexx.to_base64(content)
+  cache[filename] = base64
+  return base64
 end
 
 return {
