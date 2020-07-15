89927dbea27642cddc78a931218f2e3ce47fc0e8
Levi Gruspe <mail.levig@gmail.com>
2020-07-15 15:04:57 +0800

    Add class for storing slipbox data

--- src/model.js
+++ src/model.js
@@ -1,3 +1,175 @@
+function characterClass (character) {
+  const code = character.charCodeAt()
+  return code >= 47 && code < 58 ? 'd'
+    : code >= 97 && code < 123 ? 'a' : null
+}
+
+function isValidAlias (alias) {
+  if (alias === null) return true
+  if (typeof alias !== 'string') return false
+  if (!alias) return false
+  if (characterClass(alias.slice(0, 1)) !== 'd') return false
+  for (const c of alias) {
+    const charClass = characterClass(c)
+    if (charClass !== 'd' && charClass !== 'a') {
+      return false
+    }
+  }
+  return true
+}
+
+function aliasParent (alias) {
+  if (!isValidAlias(alias)) return null
+  if (alias === null) return null
+
+  const last = characterClass(alias.slice(-1))
+  const pattern = last === 'd' ? /^(.*?)[0-9]+$/ : /^(.*?)[a-z]+$/
+  return alias.replace(pattern, '$1') || null
+}
+
+function isSequence (prev, next) {
+  return aliasParent(next) === prev
+}
+
+class Database {
+  // Schema
+  // {
+  //   aliases: {
+  //     <alias:str>: { id: <int>, children: [<str>], parent: <str> }
+  //   },
+  //   notes: [
+  //     {
+  //       title: <str>,
+  //       aliases: [<str>],
+  //       links: [{ dest: <int>, annotation: <str> }],
+  //       backlinks: [<int>]
+  //     }
+  //   ]
+  // }
+
+  constructor () {
+    this.data = { aliases: {}, notes: [] }
+  }
+
+  add (record) {
+    return record.addTo(this)
+  }
+}
+
+class InvalidAttributeError extends Error {}
+
+function check (condition, message) {
+  if (!condition) throw new InvalidAttributeError(message)
+}
+
+class Note {
+  constructor (id, title) {
+    check(Number.isInteger(id), 'invalid Note.id')
+    check(typeof title === 'string', 'invalid Note.title')
+    check(title, 'empty Note.title')
+
+    this.id = id
+    this.title = title
+  }
+
+  addTo (db) {
+    // Overwrite existing entry in notes.
+    db.data.notes[this.id] = {
+      title: this.title,
+      aliases: [],
+      links: [],
+      backlinks: []
+    }
+  }
+}
+
+class Alias {
+  constructor (id, alias) {
+    check(Number.isInteger(id), 'non-integer Alias.id')
+    check(typeof alias === 'string', 'non-string Alias.alias')
+    check(isValidAlias(alias), 'malformed Alias.alias')
+    check(alias, 'empty alias')
+
+    this.id = id
+    this.alias = alias
+  }
+
+  addTo (db) {
+    // Overwrite existing entry in aliases.
+    // Note with ID must exist.
+    const note = db.data.notes[this.id]
+    check(note, 'invalid reference from Alias.id')
+    // should this be an exception?
+
+    db.data.aliases[this.alias] = {
+      id: this.id,
+      children: [],
+      parent: null
+    }
+    note.aliases.push(this.alias)
+  }
+}
+
+class Sequence {
+  constructor (prev, next) {
+    check(isValidAlias(prev), 'malformed Sequence.prev')
+    check(isValidAlias(next), 'malformed Sequence.next')
+    check(isSequence(prev, next),
+      'Sequence.prev and Sequence.next not in sequence')
+    check(prev != null, 'null Sequence.prev')
+    check(next != null, 'null Sequence.next')
+
+    this.prev = prev
+    this.next = next
+  }
+
+  addTo (db) {
+    const prev = db.data.aliases[this.prev]
+    const next = db.data.aliases[this.next]
+    if (!prev || !next) return
+
+    const prevNote = db.data.notes[prev.id]
+    const nextNote = db.data.notes[next.id]
+    if (!prevNote || !nextNote) return
+
+    next.parent = this.prev
+    prev.children.push(this.next)
+  }
+}
+
+class Link {
+  constructor (src, dest, annotation) {
+    console.assert(Number.isInteger(src))
+    console.assert(Number.isInteger(dest))
+    console.assert(typeof annotation === 'string')
+
+    this.src = src
+    this.dest = dest
+    this.annotation = annotation
+  }
+
+  addTo (db) {
+    if (!Number.isInteger(this.src)) return
+    if (!Number.isInteger(this.dest)) return
+    if (typeof this.annotation !== 'string') return
+
+    // src and dest notes must exist.
+    // Existing entries get overwritten.
+
+    const src = db.data.notes[this.src]
+    const dest = db.data.notes[this.dest]
+    if (!src || !dest) return
+    src.links.push({
+      dest: this.dest,
+      annotation: this.annotation
+    })
+
+    if (this.annotation) {
+      dest.backlinks.push(this.src)
+    }
+  }
+}
+
 class Query {
   constructor (data) {
     this.data = data || {
@@ -75,4 +247,14 @@ class Query {
   }
 }
 
-export { Query }
+export {
+  Alias,
+  aliasParent,
+  Database,
+  InvalidAttributeError,
+  isSequence,
+  Link,
+  Note,
+  Query,
+  Sequence
+}
--- test/model.test.js
+++ test/model.test.js
@@ -1,6 +1,263 @@
-import { Query } from '../src/model.js'
+import {
+  Alias,
+  aliasParent,
+  Database,
+  InvalidAttributeError,
+  isSequence,
+  Link,
+  Note,
+  Query,
+  Sequence
+} from '../src/model.js'
 
 import assert from 'assert'
+
+it('aliasParent', function () {
+  assert.strictEqual(aliasParent('1a'), '1')
+  assert.strictEqual(aliasParent('2'), null)
+  assert.strictEqual(aliasParent(null), null)
+  assert.strictEqual(aliasParent(undefined), null)
+  assert.strictEqual(aliasParent(''), null)
+})
+
+it('isSequence', function () {
+  assert(isSequence('1', '1a'))
+  assert(isSequence('2', '2b'))
+  assert(isSequence('3', '3abc'))
+  assert(!isSequence('4a', '4b'))
+  assert(!isSequence('5c', '5'))
+  assert(!isSequence('', '6'))
+
+  assert(isSequence(null, '7'))
+
+  assert(!isSequence('', ''))
+  assert(isSequence(null, null))
+})
+
+describe('Database', function () {
+  let db = null
+
+  beforeEach(function () {
+    db = new Database()
+  })
+
+  describe('add Note', function () {
+    describe('with invalid note attributes', function () {
+      it('should be ignored', function () {
+        db.add(new Note(0, 'title'))
+
+        assert.throws(
+          () => db.add(new Note('1', null)),
+          InvalidAttributeError)
+
+        assert.throws(
+          () => db.add(new Note(2, '')),
+          InvalidAttributeError)
+
+        assert.strictEqual(db.data.notes.length, 1)
+      })
+    })
+
+    describe('with duplicate ID', function () {
+      it('should overwrite existing entry', function () {
+        db.add(new Note(0, 'oops'))
+        db.add(new Note(0, 'yay'))
+        assert.strictEqual(db.data.notes.length, 1)
+        const note = db.data.notes[0]
+        assert(note)
+        assert.strictEqual(note.title, 'yay')
+      })
+    })
+  })
+
+  describe('add Alias', function () {
+    describe('with malformed alias', function () {
+      it('should be ignored', function () {
+        db.add(new Note(0, 'foo'))
+        db.add(new Note(1, 'bar'))
+        db.add(new Note(2, 'baz'))
+        db.add(new Alias(0, '2a'))
+        db.add(new Alias(2, '123abc'))
+
+        assert.throws(
+          () => db.add(new Alias(1, 'a1')),
+          InvalidAttributeError)
+
+        assert(db.data.notes.length === 3)
+        assert(db.data.notes[0].aliases.length === 1)
+        assert.equal(db.data.notes[1].aliases.length, 0)
+        assert(db.data.notes[2].aliases.length === 1)
+
+        assert(db.data.notes[0].aliases.includes('2a'))
+        assert(db.data.notes[2].aliases.includes('123abc'))
+      })
+    })
+
+    describe('for nonexistent note', function () {
+      it('should be ignored', function () {
+        assert.throws(
+          () => db.add(new Alias(0, '1a')),
+          InvalidAttributeError)
+        assert(db.data.notes.length === 0)
+        assert(!db.data.aliases['1a'])
+      })
+    })
+  })
+
+  describe('add Sequence', function () {
+    describe('with malformed aliases', function () {
+      it('should be ignored', function () {
+        db.add(new Note(0, 'parent'))
+        db.add(new Note(1, 'child'))
+        db.add(new Alias(0, '2'))
+        db.add(new Alias(0, '2a'))
+        db.add(new Sequence('2', '2a'))
+
+        assert.throws(
+          () => db.add(new Sequence('2', 'b')),
+          InvalidAttributeError)
+
+        assert.throws(
+          () => db.add(new Sequence(null, null)),
+          InvalidAttributeError)
+
+        assert(!db.data.aliases['b'])
+        assert.equal(db.data.aliases['2'].children.length, 1)
+        assert(db.data.aliases['2'].children.includes('2a'))
+      })
+    })
+
+    describe('with null aliases', function () {
+      it('should be ignored', function () {
+        db.add(new Note(0, 'note'))
+        db.add(new Alias(0, '2'))
+
+        assert.throws(
+          () => db.add(new Sequence(null, '2')),
+          InvalidAttributeError)
+
+        assert(db.data.aliases['2'].parent === null)
+
+        assert.throws(
+          () => db.add(new Sequence('', '2')),
+          InvalidAttributeError)
+
+        assert(db.data.aliases['2'].parent === null)
+      })
+    })
+
+    describe('with nonexistent aliases', function () {
+      it('should be ignored', function () {
+        db.add(new Sequence('0', '0a'))
+        assert(db.data.notes.length === 0)
+        assert(!db.data.aliases['0'])
+        assert(!db.data.aliases['0a'])
+      })
+    })
+
+    describe('with non-sequential aliases', function () {
+      it('should be ignored', function () {
+        db.add(new Note(0, 'parent'))
+        db.add(new Note(1, 'child'))
+        db.add(new Alias(0, '2'))
+        db.add(new Alias(1, '2a'))
+
+        assert.throws(
+          () => db.add(new Sequence('2a', '2')),
+          InvalidAttributeError)
+
+        assert.throws(
+          () => db.add(new Sequence('2', '3')),
+          InvalidAttributeError)
+
+        assert(db.data.aliases['2'].children.length === 0)
+        assert(db.data.aliases['2a'].children.length === 0)
+        assert(db.data.aliases['2'].parent === null)
+        assert(db.data.aliases['2a'].parent === null)
+      })
+    })
+
+    it('with valid aliases', function () {
+      db.add(new Note(0, 'parent'))
+      db.add(new Note(1, 'child'))
+      db.add(new Alias(0, '2'))
+      db.add(new Alias(1, '2b'))
+      db.add(new Sequence('2', '2b'))
+
+      assert(db.data.aliases['2'].id === 0)
+      assert.equal(db.data.aliases['2'].children.length, 1)
+      assert(db.data.aliases['2'].children.includes('2b'))
+      assert(db.data.aliases['2'].parent === null)
+
+      assert(db.data.aliases['2b'].id === 1)
+      assert(db.data.aliases['2b'].children.length === 0)
+      assert(db.data.aliases['2b'].parent === '2')
+    })
+  })
+
+  describe('add Link', function () {
+    describe('with malformed attributes', function () {
+      it('should be ignored', function () {
+        db.add(new Note(0, 'src'))
+        db.add(new Note(1, 'dest'))
+        db.add(new Link(0, 1, null))
+
+        db.add(new Link(0, null, 'oops'))
+        db.add(new Link(null, 1, 'oops'))
+
+        assert(db.data.notes[0].links.length === 0)
+        assert(db.data.notes[0].backlinks.length === 0)
+        assert(db.data.notes[1].links.length === 0)
+        assert(db.data.notes[1].backlinks.length === 0)
+      })
+    })
+
+    describe('between nonexistent notes', function () {
+      it('should be ignored', function () {
+        db.add(new Note(0, 'note'))
+
+        db.add(new Link(0, 1, 'oops'))
+        db.add(new Link(1, 0, 'oops'))
+
+        assert(db.data.notes[0].links.length === 0)
+        assert(db.data.notes[0].backlinks.length === 0)
+      })
+    })
+
+    describe('without annotation', function () {
+      it('should not generate backlink', function () {
+        db.add(new Note(0, 'src'))
+        db.add(new Note(1, 'dest'))
+        db.add(new Link(0, 1, ''))
+
+        assert(db.data.notes[0].links.length === 1)
+        assert(db.data.notes[0].links[0].dest === 1)
+        assert(db.data.notes[0].links[0].annotation === '')
+
+        assert(db.data.notes[0].backlinks.length === 0)
+        assert(db.data.notes[1].backlinks.length === 0)
+      })
+    })
+
+    describe('with annotation', function () {
+      it('should generate backlink', function () {
+        db.add(new Note(0, 'src'))
+        db.add(new Note(1, 'dest'))
+        db.add(new Link(0, 1, 'annotation'))
+
+        assert(db.data.notes[0].links.length === 1)
+        assert(db.data.notes[0].links[0].dest === 1)
+        assert(db.data.notes[0].links[0].annotation === 'annotation')
+
+        assert(db.data.notes[1].links.length === 0)
+
+        assert(db.data.notes[0].backlinks.length === 0)
+        assert(db.data.notes[1].backlinks.length === 1)
+        assert(db.data.notes[1].backlinks.includes(0))
+      })
+    })
+  })
+})
 
 describe('Query', function () {
   const data = { aliases: {}, notes: {} }
