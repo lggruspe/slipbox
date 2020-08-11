50a940e2ed8d58eb279f7933747bec38e8f77966
Levi Gruspe <mail.levig@gmail.com>
2020-08-11 14:37:51 +0800

    Test modified_notes and deleted_notes

--- slipbox/test_slipbox.py
+++ slipbox/test_slipbox.py
@@ -7,7 +7,7 @@ from sys import version_info
 import pytest
 
 from .config import Config
-from .slipbox import Slipbox, added_notes
+from .slipbox import Slipbox, added_notes, modified_notes, deleted_notes
 from .utils import insert_file_script
 
 @pytest.mark.skipif(version_info.minor < 8, reason="requires python3.8")
@@ -76,3 +76,49 @@ def test_slipbox_context_manager(tmp_path):
         assert slipbox.timestamp == 0.0
     with Slipbox(config) as slipbox:
         assert slipbox.timestamp != 0.0
+
+@pytest.mark.skipif(version_info.minor < 8, reason="requires python3.8")
+def test_modified_notes(tmp_path):
+    """modified_notes must exclude the following notes:
+
+    - Notes that aren't in the database
+    - Notes in the database that haven't been modified
+    """
+    modified = tmp_path/"modified.md"
+    not_modified = tmp_path/"not_modified.md"
+    added = tmp_path/"added.md"
+
+    config = Config(database=tmp_path/"slipbox.db")
+    config.database.unlink(missing_ok=True)
+    slipbox = Slipbox(config)
+    slipbox.config.paths = (tmp_path,)
+
+    slipbox.conn.executescript(insert_file_script(modified, not_modified))
+
+    added.touch()
+    modified.touch()
+
+    assert modified_notes(slipbox) == [modified]
+
+@pytest.mark.skipif(version_info.minor < 8, reason="requires python3.8")
+def test_deleted_notes(tmp_path):
+    """deleted_notes must exclude the following files:
+
+    - Notes not in the database
+    - Notes in the database that still exist in the file system
+    """
+    delete = tmp_path/"delete.md"
+    dont_delete = tmp_path/"dont_delete.md"
+    added = tmp_path/"not_in_db.md"
+
+    config = Config(database=tmp_path/"slipbox.db")
+    config.database.unlink(missing_ok=True)
+    slipbox = Slipbox(config)
+    slipbox.config.paths = (tmp_path,)
+
+    slipbox.conn.executescript(insert_file_script(delete, dont_delete))
+
+    dont_delete.touch()
+    added.touch()
+
+    assert deleted_notes(slipbox) == [delete]
