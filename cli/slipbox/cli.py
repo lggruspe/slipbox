d53ae29c5c211636c1f52e1280c89d27f7af28d0
Levi Gruspe <mail.levig@gmail.com>
2021-05-06 16:01:04 +0800

    Update cli deps

--- cli/slipbox/cli.py
+++ cli/slipbox/cli.py
@@ -1,15 +1,20 @@
 """Slipbox CLI."""
 
-from climates import Climate
+from climux import Cli, Command
 from .app import (check_notes, generate_flashcards, main, show_info_wrapper,
                   initialize, new_note)
 
-cli = Climate("slipbox", "Generate a single-page HTML from your notes.")
-cli.add_commands(
-    build=main,
-    check=check_notes,
-    flashcards=generate_flashcards,
-    info=show_info_wrapper,
-    init=initialize,
-    new=new_note,
-)
+
+commands = [
+    Command(main, alias="build"),
+    Command(check_notes, alias="check"),
+    Command(generate_flashcards, alias="flashcards"),
+    Command(show_info_wrapper, alias="info"),
+    Command(initialize, alias="init"),
+    Command(new_note, alias="new"),
+]
+cli = Cli("slipbox", "Generate a static website from your notes.")
+
+for command in commands:
+    command.show_result = False
+    cli.add(command)
