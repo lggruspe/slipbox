ebf7b999b8b0f04ae11d00b73d208ed62aff8c47
Levi Gruspe <mail.levig@gmail.com>
2022-02-28 18:17:12 +0300

    Test init command

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,38 +1,59 @@
 """Test commands.py."""
 
+from configparser import ConfigParser
 from pathlib import Path
 import pytest
+
 from slipbox import commands
-from slipbox.initializer import DotSlipbox
-from slipbox.slipbox import Slipbox
-from slipbox.utils import check_requirements
+from slipbox.app import App, startup
+from slipbox.build import build
+from slipbox.dependencies import check_requirements
 
 
-@pytest.mark.skipif(not check_requirements(), reason="requires pandoc")
-def test_show_info_empty_stdout(tmp_path: Path,
-                                capsys: pytest.CaptureFixture[str],
-                                mnote: Path,
-                                sbox: Slipbox,
-                                ) -> None:
-    """show_info shouldn't output anything if the note ID doesn't exist."""
-    sbox.process([mnote])
-    with Slipbox(DotSlipbox(tmp_path)) as slipbox:
-        commands.show_info(slipbox, 1)
+# TODO test output if note does not exist
+@pytest.mark.skipif(not check_requirements(startup({})),
+                    reason="missing requirements")
+def test_show_info_in_stdout(capsys: pytest.CaptureFixture[str],
+                             test_note: Path,
+                             test_app_with_root: App,
+                             ) -> None:
+    """show_info should output note info in stdout."""
+    app = test_app_with_root
+
+    build(app)
+    app.args = {"note_id": 0}
+    commands.show_info(app)
+    stdout, stderr = capsys.readouterr()
+    assert stdout
+    assert not stderr
+
+
+def test_init_creates_config_file(test_app: App) -> None:
+    """init must create .slipbox/config.cfg."""
+    commands.init(test_app)
+    hidden = test_app.root/".slipbox"
+    assert hidden.is_dir()
+    assert hidden.joinpath("config.cfg").is_file()
+
+
+def test_init_quiet(test_app: App,
+                    capsys: pytest.CaptureFixture[str],
+                    ) -> None:
+    """init must not print anything in quiet mode."""
+    test_app.args["quiet"] = True
+    commands.init(test_app)
     stdout, stderr = capsys.readouterr()
     assert not stdout
     assert not stderr
 
 
-@pytest.mark.skipif(not check_requirements(), reason="requires pandoc")
-def test_show_info_stdout(tmp_path: Path,
-                          capsys: pytest.CaptureFixture[str],
-                          mnote: Path,
-                          sbox: Slipbox,
-                          ) -> None:
-    """show_info should output info of note with give note ID."""
-    sbox.process([mnote])
-    with Slipbox(DotSlipbox(tmp_path)) as slipbox:
-        commands.show_info(slipbox, 0)
-    stdout, stderr = capsys.readouterr()
-    assert stdout
-    assert not stderr
+def test_init_patterns(test_app: App) -> None:
+    """.slipbox/config.cfg must contain some glob patterns."""
+    commands.init(test_app)
+    hidden = test_app.root/".slipbox"
+
+    parser = ConfigParser()
+    parser.read(hidden/"config.cfg")
+
+    assert parser.getboolean("note-patterns", "*.md")
+    assert parser.getboolean("note-patterns", "*.rst")
