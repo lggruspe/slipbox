e2d75ceedeb2452d7d69d55bd7ccc8de63f63320
Levi Gruspe <mail.levig@gmail.com>
2022-02-28 12:51:15 +0300

    Add new args parser

---
+++ slipbox/cli2.py
@@ -0,0 +1,50 @@
+"""Slipbox CLI parser."""
+
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+import typing as t
+
+
+def parse_args() -> t.Dict[str, t.Any]:
+    """Returns dict of command-line options and arguments.
+
+    Name of subcommand can be accessed using the key: "$subcommand".
+    """
+    epilog = """
+  build     Build website.
+  check     Check for isolated notes and invalid links.
+  info      Show note info.
+  init      Initialize notes directory.
+  new       Get unused note IDs.
+"""
+
+    parser = ArgumentParser(
+        prog="slipbox",
+        description="Generate a static website from your notes.",
+        formatter_class=RawDescriptionHelpFormatter,
+        epilog=epilog,
+    )
+    parser.add_argument("-v", "--version", action="store_true",
+                        help="show version number and exit")
+
+    # Add subcommands
+    subparsers = parser.add_subparsers(title="commands", dest="$subcommand")
+
+    subparsers.add_parser("build", description="Build website.")
+    subparsers.add_parser(
+        "check",
+        description="Check for isolated notes and invalid links.",
+    )
+
+    subparser = subparsers.add_parser("info", description="Show note info.")
+    subparser.add_argument("note_id", type=int)
+
+    subparsers.add_parser("init", description="Initialize notes directory.")
+
+    subparser = subparsers.add_parser("new",
+                                      description="Get unused note IDs.")
+    subparser.add_argument("-n", type=int, default=1,
+                           help="number of note IDs to generate")
+    return vars(parser.parse_args())
+
+
+__all__ = ["parse_args"]
