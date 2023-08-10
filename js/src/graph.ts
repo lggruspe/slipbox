4a48acd7e6d93a5351ea6a9bd0f1cf4239834e10
Levi Gruspe <mail.levig@gmail.com>
2023-08-10 17:31:42 +0800

    Update graph style

--- js/src/graph.ts
+++ js/src/graph.ts
@@ -1,5 +1,4 @@
 import cytoscape from "cytoscape";
-import { Core, EventHandler } from "cytoscape";
 import fcose from "cytoscape-fcose";
 
 import { getRoute } from "./route";
@@ -16,18 +15,147 @@ declare const MathJax: {
 
 declare global {
   interface Window {
-    cy: Core | undefined;
+    cy: cytoscape.Core | undefined;
   }
 }
 
-function createLayout(): cytoscapeFcose.FcoseLayoutOptions {
-  return {
-    name: "fcose",
-    animate: true,
-    fit: true,
-    nodeDimensionsIncludeLabels: true,
-  };
-}
+/**
+ * Cytoscape graph layout options.
+ */
+const layout: cytoscapeFcose.FcoseLayoutOptions = {
+  name: "fcose",
+  animate: true,
+  fit: true,
+  nodeDimensionsIncludeLabels: true,
+  idealEdgeLength: 100,
+};
+
+/**
+ * Common rules between different stylesheets.
+ */
+const stylesheetCommons: cytoscape.Stylesheet[] = [
+  // Node styling
+  {
+    selector: "node",
+    style: {
+      label: "data(title)",
+      "background-color": "gray",
+      "text-wrap": "wrap",
+      "text-max-width": "120",
+    },
+  },
+  {
+    selector: "node.blur",
+    style: {
+      "background-opacity": 0.5,
+      "text-opacity": 0.5,
+    },
+  },
+  {
+    selector: "node.sharp",
+    style: {
+      "background-color": "cornflowerblue",
+      "background-opacity": 1,
+      "text-opacity": 1,
+    },
+  },
+  {
+    selector: "node:selected",
+    style: {
+      "background-color": "blue",
+      "background-opacity": 1,
+      "text-opacity": 1,
+    },
+  },
+
+  // Edge styling
+  {
+    selector: "edge",
+    style: {
+      width: "3px",
+      "curve-style": "straight",
+      "line-color": "gray",
+      "line-opacity": 0.5,
+    },
+  },
+  {
+    selector: "edge.blur",
+    style: {
+      "line-opacity": 0.2,
+    },
+  },
+  {
+    selector: "edge.sharp",
+    style: {
+      "line-color": "cornflowerblue",
+      "line-opacity": 0.8,
+    },
+  },
+
+  // Z-indices
+  {
+    selector: "node.blur",
+    style: {
+      "z-index": 101,
+    },
+  },
+  {
+    selector: "edge.blur",
+    style: {
+      "z-index": 100,
+    },
+  },
+  {
+    selector: "node.sharp",
+    style: {
+      "z-index": 201,
+    },
+  },
+  {
+    selector: "edge.sharp",
+    style: {
+      "z-index": 200,
+    },
+  },
+  {
+    selector: "node:selected",
+    style: {
+      "z-index": 301,
+    },
+  },
+  {
+    selector: "edge:selected",
+    style: {
+      "z-index": 300,
+    },
+  },
+];
+
+/**
+ * Cytoscape graph stylesheet for directed graphs.
+ */
+const stylesheetDirected: cytoscape.Stylesheet[] = [
+  ...stylesheetCommons,
+  {
+    selector: "edge",
+    style: {
+      "arrow-scale": 2,
+      "target-arrow-color": "gray",
+      "target-arrow-shape": "triangle",
+    },
+  },
+  {
+    selector: "edge.sharp",
+    style: {
+      "target-arrow-color": "cornflowerblue",
+    },
+  },
+];
+
+/**
+ * Cytoscape graph stylesheet for undirected graphs.
+ */
+const stylesheetUndirected: cytoscape.Stylesheet[] = [...stylesheetCommons];
 
 // Returns graph dialog and rename hook for renaming label.
 function createGraphDialog(): [
@@ -53,41 +181,33 @@ function createGraphDialog(): [
   return [dialog, rename];
 }
 
+/**
+ * Recolor elements in graph.
+ */
+function recolor(cy: cytoscape.Core) {
+  const selected = cy.elements(":selected");
+  cy.elements().removeClass("blur sharp");
+  if (selected.length > 0) {
+    cy.elements().addClass("blur");
+    selected.openNeighborhood().addClass("sharp");
+  }
+}
+
 function createCytoscape(
   container: HTMLElement,
   data: GraphSchema,
-  selectCallback: EventHandler,
-): Core {
+  selectCallback: cytoscape.EventHandler,
+  directed: boolean,
+): cytoscape.Core {
   const cy = cytoscape({
     container,
-    style: [
-      {
-        selector: "node:selected",
-        style: {
-          "background-color": "#9333ea",
-        },
-      },
-      {
-        selector: "node",
-        style: {
-          label: "data(title)",
-          "text-wrap": "wrap",
-          "text-max-width": "120",
-        },
-      },
-      {
-        selector: "edge",
-        style: {
-          "curve-style": "bezier",
-          "target-arrow-shape": "triangle",
-        },
-      },
-    ],
-    layout: createLayout(),
+    layout,
+    style: directed ? stylesheetDirected : stylesheetUndirected,
+    selectionType: "additive",
     ...data,
   });
 
-  /// Update titles
+  // Update math in titles.
   cy.nodes().forEach((ele) => {
     const div = document.createElement("div");
     div.innerHTML = ele.data("title");
@@ -95,7 +215,14 @@ function createCytoscape(
     ele.data("title", div.textContent);
   });
 
-  if (selectCallback) cy.on("select", "node", selectCallback);
+  // Keep track of number of selected nodes.
+  cy.on("select", "node", (event) => {
+    if (selectCallback) {
+      selectCallback(event);
+    }
+    recolor(event.target.cy());
+  });
+  cy.on("unselect", "node", (event) => recolor(event.target.cy()));
   return cy;
 }
 
@@ -150,18 +277,22 @@ function createDialogLabel(
 
 export function initGraphButton(button: HTMLButtonElement) {
   const [dialog, rename] = createGraphDialog();
+  const container = dialog.querySelector("div") as HTMLDivElement;
+
   button.insertAdjacentElement("afterend", dialog);
+
   button.addEventListener("click", async () => {
     dialog.show();
 
-    const container = dialog.querySelector("div") as HTMLDivElement;
+    const graph = await fetchJson<GraphSchema>(getGraphDataURL());
     const cy = createCytoscape(
       container,
-      await fetchJson<GraphSchema>(getGraphDataURL()),
+      graph,
       (event) => {
         const { title, path } = event.target.data();
         rename(title, path);
       },
+      graph.directed || false,
     );
 
     let id = window.location.hash;
@@ -178,6 +309,6 @@ export function initGraphButton(button: HTMLButtonElement) {
       renderedPosition: node.renderedPosition(),
     });
 
-    window.cy = cy as Core; // expose cy
+    window.cy = cy as cytoscape.Core; // expose cy
   });
 }
